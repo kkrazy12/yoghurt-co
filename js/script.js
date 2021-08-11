abe5694bba778643c8ed9e36d2f44da4efde500b
kaye richards <kayejeanrichards@gmail.com>
2021-08-11 10:44:35 +0100

    Finished website

--- js/script.js
+++ js/script.js
@@ -1,32 +1,29 @@
-console.log('Loaded'); //ensures the JS file is linked to our HTMl correctly
+console.log('Loaded'); //ensures the JS file is linked to my HTMl correctly
 
 var btn = document.querySelector('.mobileNavBTN');
 var menu = document.querySelector('.menu');
 // creating a variable called button, 
-//we then look into the DOM and use query selector which will select our class, tag, id etc and use that element.
-//Currently, our mobile nav menu is being selected so that we can create a burger for the mobile version of our wesbite. 
-//console.log(btn); using the console log to make sure the nav menu is selected correctly and showing on the console on our browser
+// then look into the DOM and use query selector which will select my class, tag, id etc and use that element.
+//Currently, my mobile nav menu is being selected so that I can create a burger for the mobile version of my wesbite. 
+//console.log(btn); using the console log to make sure the nav menu is selected correctly and showing on the console on my browser
 
 btn.addEventListener('click', () => {
     menu.classList.toggle('hide');
 });
-//we are making the button listen out for an event so that it works correctly. In this case, the event we are waiting for is 
+//I am making the button listen out for an event so that it works correctly. In this case, the event I am waiting for is 
 //when the user clicks the button. Once the button is clicked, the fat arrow syntax tells the code that we are firing a 
-//callback fucntion. Whatever is definied in between the fat arrow syntax is what we want to happen. We also used a console log
-//so that we can make sure the code is working. 
+//callback fucntion. Whatever is definied in between the fat arrow syntax is what I want to happen. I also used a console log
+//so that I can make sure the code is working. 
 
 window.addEventListener('resize', () => {
     if (window.innerWidth > 420) { //If the window width of the screen is greater than 420 then add hide to the button to show the 
         btn.classList.add('hide'); //menu list.
-        menu.classList.remove('hide'); // We then remove hide from the menu because on screens greater than 420, we want to show 
+        menu.classList.remove('hide'); // Then remove hide from the menu because on screens greater than 420, I want to show 
         //the full menu.
     } else if (window.innerWidth < 420) {
-        btn.classList.remove('hide'); //We are removing hide from the button/burger so that we can see it on screens smaller than 420
-        menu.classList.add('hide'); //We are adding hide to the menu because we are using a burger for screens smaller than 420 so 
+        btn.classList.remove('hide'); //I am removing hide from the button/burger so that I can see it on screens smaller than 420
+        menu.classList.add('hide'); //I am adding hide to the menu because I am using a burger for screens smaller than 420 so 
         //the menu will not be needed.
 
     }
 })
-
-//code for the fact cards on the vegan lifestyle page starts here//
-
